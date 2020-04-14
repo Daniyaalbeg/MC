6bde82173950227389e4c06a6796e270af54927f
Kaamil Jasani <kaamilj@yahoo.com>
2020-04-14 22:56:17 +0100

    Improved accordions and fixed accordion maps

--- src/components/loginAccountComponents/rationItemCard.component.js
+++ src/components/loginAccountComponents/rationItemCard.component.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import { Accordion, Card } from 'react-bootstrap';
-import { Button } from 'react-bootstrap';
 import Badge from 'react-bootstrap/Badge';
 import RationItemInfo from './rationItemInfo.component';
 import { faAngleDown, faAngleUp } from '@fortawesome/free-solid-svg-icons';
@@ -11,17 +10,15 @@ const RationItemCard = (props) => {
   const dateOptions = { weekday: "long", year: "numeric", month: "short", day: "numeric" };
   return (
     <Card className="rationListCard">
-      <Card.Header className="rationCardInfo justify-content-between">
+      <Accordion.Toggle as={Card.Header} className="rationCardInfo justify-content-between" eventKey={ration._id}>
         <div>
           <Card.Title> {ration.name} </Card.Title>
           <Card.Subtitle className="text-muted">
             {new Date(ration.date).toLocaleDateString("en-US", dateOptions)} <Badge variant={ration.approved ? "success" : "danger"}> {ration.approved ? "Approved" : "Pending Approval"} </Badge>
           </Card.Subtitle>
         </div>
-        <Accordion.Toggle as={Button} variant="light" eventKey={ration._id} className="rationCardButton" id={ration._id}>
-          <FontAwesomeIcon icon={props.open ? faAngleUp : faAngleDown}/>
-        </Accordion.Toggle>
-      </Card.Header>
+        <FontAwesomeIcon icon={props.open ? faAngleUp : faAngleDown}/>
+      </Accordion.Toggle>
       <Accordion.Collapse eventKey={ration._id}>
         <Card.Body>
           <RationItemInfo ration={ration}/>
