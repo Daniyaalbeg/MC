ab16ca54c6bfbfe3494fb99c3fd6a3d543c04cfb
Daniyaalbeg <danyaalbeg@gmail.com>
2020-05-03 17:35:39 +0500

    Changed orgview cards to be fixed height to make flawless grid

--- src/components/organisationsView.component.js
+++ src/components/organisationsView.component.js
@@ -8,7 +8,6 @@ import { faGlobe } from '@fortawesome/pro-solid-svg-icons'
 import { faSearch, faFilter } from '@fortawesome/pro-light-svg-icons';
 import { filterAndSearchOrg } from '../components/homeView/filterAndSearch';
 import { filterOrgEvents, searchOrgEvents} from '../Actions/filterSearchOrgAction';
-// import { BankingDetails, EasyPaisaDetails, JazzCashDetails, SocialMediaIcons, WhichLogo } from './loginAccountComponents/supplierInfoView.component';
 
 import imagePlaceholder from '../assets/Images/temp.jpg'
 import '../css/organisationsView.css';
@@ -66,7 +65,6 @@ const OrganisationsView = ({ dispatch, loading, hasErrors, fetched, orgInfo }) =
       </form>
 			</div>
 			<OrgCards orgs={orgInfo} dispatch={dispatch} />
-			{/* <OrgCardsOrInfo selectedOrg={selectedOrg} orgs={orgInfo} dispatch={dispatch} /> */}
 			</Fragment>
 		)
 	}
@@ -79,84 +77,6 @@ const OrganisationsView = ({ dispatch, loading, hasErrors, fetched, orgInfo }) =
 		</div>
   )
 }
-
-// const OrgCardsOrInfo = (props) => {
-// 	if (props.selectedOrg === null) {
-// 		return <OrgCards orgs={props.orgs} dispatch={props.dispatch} />
-// 	} else {
-// 		return (
-// 			<div className="orgInfoCardContainer">
-// 				<OrgInfo org={props.selectedOrg} dispatch={props.dispatch} />
-// 			</div>
-// 		)
-// 	}
-// }
-
-// const OrgInfo = (props) => {
-// 	const org = props.org
-
-// 	return (
-// 		<Card className="orgInfoCard">
-// 			<Card.Body>
-// 				<Link className="orgViewLink" to="/organisations">
-// 				<Button className="backButtonOrgView" onClick={() => {
-// 				}}> Back </Button></Link>
-// 				<Card.Img className="orgInfoImage" variant="top" src={org.supplierImageURL !== undefined ? org.supplierImageURL : imagePlaceholder} alt=""/>
-// 				<hr />
-// 				<Card.Title> {org.supplierName} </Card.Title>
-// 				<Card.Subtitle> {org.type} </Card.Subtitle>
-// 				<hr />
-// 				<h6 className="text-muted"> Description </h6>
-// 				<Card.Text> {org.description} </Card.Text>
-// 				<hr />
-// 				<h6 className="text-muted"> Type of Work </h6>
-// 				<div>
-// 					{org.areaOfWork.map((area) => {
-// 						return (
-// 							<Badge pill variant="primary" className="pillBadge">
-// 								{area}
-// 							</Badge>
-// 						)
-// 					})}
-// 				</div>
-// 				<hr />
-// 				<h6 className="text-muted"> Address </h6>
-// 				<Card.Text> {org.address} </Card.Text>
-// 				<Card.Text>  </Card.Text>
-// 				<hr />
-// 				<h6 className="text-muted"> Donation Info </h6>
-// 				{org.bankingDetails.accountNumber !== "" &&
-// 					<BankingDetails bankingDetails={org.bankingDetails}/>
-// 				}
-// 				{org.bankingDetails.easyPaisa !== "" &&
-// 					<EasyPaisaDetails easyPaisa={org.bankingDetails.easyPaisa} />
-// 				}
-// 				{org.bankingDetails.jazzCash !== "" &&
-// 					<JazzCashDetails jazzCash={org.bankingDetails.jazzCash}/>
-// 				}
-// 				<hr />
-// 				<h6 className="text-muted"> Point of Contact </h6>
-// 				<Card.Text> {org.contactName} </Card.Text>
-// 				<Card.Text> {org.contactNumber} </Card.Text>
-// 				<hr />
-// 				<h6 className="text-muted"> Other Contact Info </h6>
-// 				<Card.Text> {org.contactInfo} </Card.Text>
-// 				<hr />
-// 				{org.supplierWebsite !== "" && (
-//         <Fragment>
-//         <h6 className="text-muted"> Website </h6>
-//         <a href={org.supplierWebsite} key={org.supplierWebsite} target="_blank" rel="noopener noreferrer" className="icon"> <FontAwesomeIcon icon={faGlobe}/></a>
-//         </Fragment>
-//       )
-//       }
-//       <SocialMediaIcons supplier={org} />
-//       <WhichLogo icon={org.facebookURL} />
-//       <WhichLogo icon={org.twitterURL} />
-//       <WhichLogo icon={org.instagramURL} />
-// 			</Card.Body>
-// 		</Card>
-// 	)
-// }
 
 const OrgCards = (props) => {
 	return (
