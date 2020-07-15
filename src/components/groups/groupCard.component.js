223114e7f37fe3efbd9c3b27b3a9da206c31fded
Daniyaalbeg <danyaalbeg@gmail.com>
2020-07-16 02:31:18 +0500

    Fixed some cetering issues on groups

--- src/components/groups/groupCard.component.js
+++ src/components/groups/groupCard.component.js
@@ -3,10 +3,10 @@ import imagePlaceholder from '../../assets/Images/temp.jpg';
 
 const GroupCard = ({ group }) => {
   return (
-    <>
+    <div>
       {group.groupImage ? <img src={group.groupImage} alt="" /> : <img src={imagePlaceholder} alt="" /> }
       <p> {group.groupName} </p>
-    </>
+    </div>
   )
 }
 
