71f43e16182b47348b099673bb3baf57296b4a9c
Daniyaalbeg <danyaalbeg@gmail.com>
2020-07-16 20:41:57 +0500

    Change intro text and small css and spelling fixes

--- src/components/homeView/homeView.component.js
+++ src/components/homeView/homeView.component.js
@@ -87,7 +87,15 @@ const IntroText = () => {
   return (
     <section className="introText">
       <p>
-        Dear Changemakers, we are finally launching the Ministry of Change Beta. Please click the sign up button to sign on. This is only the first phase of launch and a work in progress. I would love for you to ask your friends who are activists to get on board and give us feedback. It has been a labour of love of decades. We need to make sure we are doing it right. Please take some time to explore. A video of how to use the site is in production. Whilst there do go and like the <a target="_blank" rel="noopener noreferrer" href="https://www.facebook.com/ministryochange"> facebook </a> page.
+        The future of activism is here. Ministry of Change (MC) is a digital platform that match-makes those in need and those with resources.
+        On MC you can map your impact, find others like you, build on each other's strength, mobilise people for action and crowdsource money, supplies and volunteers.
+        You could be an individual citizen, a government, a corporation, or a philanthropist.
+        Browse through development projects that inspire and excite you. 
+        In these challenging COVID 19 times, we are mapping donors who are donating food rations, PPE (Personal Protective Equipment), cash, and clothes to people who need them. 
+        Many are already engaged in Whatsapp groups. Post your own and join others.
+        <br />
+        <br />
+        We are ready to change the world. Are you?
       </p>
     </section>
   )
@@ -128,7 +136,7 @@ const MapIconKey = (props) => {
       </div>
       <div className="iconKey">
         <img className="iconKeyImage" src={mask} alt={imagePlaceholder}/>
-        <p> PPE </p>
+        <p> Protection Equipment </p>
       </div>
       <div className="iconKey">
         <img className="iconKeyImage" src={shirt} alt={imagePlaceholder}/>
