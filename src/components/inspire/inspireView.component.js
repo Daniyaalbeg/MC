75c279b3d6b20bb047d1ecf17b0d82e6c47cf471
Daniyaalbeg <danyaalbeg@gmail.com>
2020-07-21 17:46:37 +0500

    Inspire basics

---
+++ src/components/inspire/inspireView.component.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const InspireView = () => {
+  return (
+    <div className="inspireContainer">
+      <div className="inspireHeader">
+        <p> <span className="mcRed">I</span> <span className="mcOrange">N</span> <span className="mcYellow">S</span> <span className="mcGreen">P</span> <span className="mcBlue">I</span> <span className="mcGrey">R</span> <span className="mc">E</span> </p>
+      </div>
+      <div className="inspireContent">
+        
+      </div>
+    </div>
+  )
+}
+
+export default InspireView
