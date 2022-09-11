00c34a0612b2a5a2ca9cd9a8375868d7ccad879a
Daniyaal Beg <danyaalbeg@gmail.com>
2022-09-12 02:20:30 +0500

    Fixed volunteer stuff again

--- src/components/volunteers/MainVolunteerViewContent.component.js
+++ src/components/volunteers/MainVolunteerViewContent.component.js
@@ -1,73 +1,74 @@
-import React, { useEffect } from "react";
-import { connect } from "react-redux";
+import React, { useEffect } from 'react';
+import { connect } from 'react-redux';
 
-import VolunteerCard from "./mainVolunteerCard.component";
-import LoadingSpinner from "../utilities/loadingSpinner.component";
+import VolunteerCard from './mainVolunteerCard.component';
+import LoadingSpinner from '../utilities/loadingSpinner.component';
 
-import { getVolunteers } from "../../Actions/volunteerActions";
-import { filterAndSearchProject } from "../utilities/filterAndSearch";
+import { getVolunteers } from '../../Actions/volunteerActions';
+import { filterAndSearchProject } from '../utilities/filterAndSearch';
 
 const MainVolunteerViewContent = ({
-  props,
-  dispatch,
-  volunteers,
-  loading,
-  hasErrors,
-  fetched,
+	props,
+	dispatch,
+	volunteers,
+	loading,
+	hasErrors,
+	fetched,
 }) => {
-  useEffect(() => {
-    dispatch(getVolunteers());
-  }, []);
+	console.log(volunteers);
+	useEffect(() => {
+		dispatch(getVolunteers());
+	}, []);
 
-  function clickVolunteer(id) {
-    props.history.push(`/volunteers/${id}`);
-  }
+	function clickVolunteer(id) {
+		props.history.push(`/volunteers/${id}`);
+	}
 
-  if (fetched && !loading && volunteers.length === 0) {
-    return (
-      <p
-        style={{
-          width: "100%",
-          height: "400px",
-          margin: "auto",
-          textAlign: "center",
-        }}
-      >
-        {" "}
-        No Volunteers Yet{" "}
-      </p>
-    );
-  }
+	if (fetched && !loading && volunteers.length === 0) {
+		return (
+			<p
+				style={{
+					width: '100%',
+					height: '400px',
+					margin: 'auto',
+					textAlign: 'center',
+				}}
+			>
+				{' '}
+				No Volunteers Yet{' '}
+			</p>
+		);
+	}
 
-  if (fetched && !loading) {
-    return (
-      <div className="grid lg:grid-cols-3 md:grid-cols-2 sm:grid-cols-1 gap-4 pb-8 px-4 sm:px-2">
-        {volunteers.map((volunteer, index) => {
-          return (
-            <VolunteerCard
-              key={index}
-              volunteer={volunteer}
-              onClick={clickVolunteer}
-            />
-          );
-        })}
-      </div>
-    );
-  }
+	if (fetched && !loading) {
+		return (
+			<div className="grid lg:grid-cols-3 md:grid-cols-2 sm:grid-cols-1 gap-4 pb-8 px-4 sm:px-2">
+				{volunteers.map((volunteer, index) => {
+					return (
+						<VolunteerCard
+							key={index}
+							volunteer={volunteer}
+							onClick={clickVolunteer}
+						/>
+					);
+				})}
+			</div>
+		);
+	}
 
-  if (hasErrors) {
-    return <p> An error occurred</p>;
-  }
+	if (hasErrors) {
+		return <p> An error occurred</p>;
+	}
 
-  return <LoadingSpinner />;
+	return <LoadingSpinner />;
 };
 
 const MapStateToProps = (state, ownProps) => ({
-  loading: state.volunteerInfo.mainVolunteers.loading,
-  hasErrors: state.volunteerInfo.mainVolunteers.hasErrors,
-  fetched: state.volunteerInfo.mainVolunteers.fetched,
-  volunteers: state.volunteerInfo.mainVolunteers.volunteers, //filterAndSearchProject(state.projectInfo.mainProject.projects, state.projectInfo.mainProject.filterTerms, state.projectInfo.mainProject.searchTerm),
-  props: ownProps.props,
+	loading: state.volunteerInfo.mainVolunteers.loading,
+	hasErrors: state.volunteerInfo.mainVolunteers.hasErrors,
+	fetched: state.volunteerInfo.mainVolunteers.fetched,
+	volunteers: state.volunteerInfo.mainVolunteers.volunteers, //filterAndSearchProject(state.projectInfo.mainProject.projects, state.projectInfo.mainProject.filterTerms, state.projectInfo.mainProject.searchTerm),
+	props: ownProps.props,
 });
 
 export default connect(MapStateToProps)(MainVolunteerViewContent);
--- src/components/volunteers/volunteerImage.component.js
+++ src/components/volunteers/volunteerImage.component.js
@@ -1,45 +1,43 @@
-import React from "react";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faUserAlt } from "@fortawesome/pro-solid-svg-icons";
+import React from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faUserAlt } from '@fortawesome/pro-solid-svg-icons';
 
-const ImageComponent = ({ user, image, size }) => {
-  let imageComponent = null;
-  let scale = null;
-  if (size === "large") {
-    scale = "h-32 w-32 sm:h-48 sm:w-48 ";
-  } else {
-    scale = "h-24 w-24 sm:h-32 sm:w-32 ";
-  }
-  if (image) {
-    imageComponent = (
-      <img
-        className={
-          scale +
-          " rounded-full object-cover shadow-sm border-4 border-white border-solid"
-        }
-        // src={image}
-        src={user.picture.large}
-      />
-    );
-  } else {
-    imageComponent = (
-      <div
-        className={
-          scale +
-          " rounded-full flex-grow overflow-hidden flex items-center justify-center border-4 border-white border-solid bg-gray-700"
-        }
-      >
-        <FontAwesomeIcon
-          // className="shadow-lg"
-          icon={faUserAlt}
-          size={size === "large" ? "4x" : "2x"}
-          color="white"
-        />
-      </div>
-    );
-  }
+const ImageComponent = ({ image, size }) => {
+	let imageComponent = null;
+	let scale = null;
+	if (size === 'large') {
+		scale = 'h-32 w-32 sm:h-48 sm:w-48 ';
+	} else {
+		scale = 'h-24 w-24 sm:h-32 sm:w-32 ';
+	}
+	if (image) {
+		imageComponent = (
+			<img
+				className={
+					scale +
+					' rounded-full object-cover shadow-sm border-4 border-white border-solid'
+				}
+				src={image}
+			/>
+		);
+	} else {
+		imageComponent = (
+			<div
+				className={
+					scale +
+					' rounded-full flex-grow overflow-hidden flex items-center justify-center border-4 border-white border-solid bg-gray-700'
+				}
+			>
+				<FontAwesomeIcon
+					icon={faUserAlt}
+					size={size === 'large' ? '4x' : '2x'}
+					color="white"
+				/>
+			</div>
+		);
+	}
 
-  return imageComponent;
+	return imageComponent;
 };
 
 export default ImageComponent;
